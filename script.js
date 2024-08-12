a697daabee3fbce6528cfc18339c4a5b0370e6ac
le-fabwan974 <126337290+le-fabwan974@users.noreply.github.com>
2024-08-12 13:32:53 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function revealMessage() {
+    document.getElementById("message").style.display = "block";
+}
+
+function revealImage() {
+    document.getElementById("image").style.display = "block";
+}
+
+function revealText() {
+    document.getElementById("text").style.display = "block";
+}
